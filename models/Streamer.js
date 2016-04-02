7ca6977f612bb7932ad572ded672124006233dd3
QuentinG3 <QG3393@hotmail.com>
2016-04-02 18:10:29 +0200

    Adding summonersName to Streamer + filling streaming with summonersName

--- models/Streamer.js
+++ models/Streamer.js
@@ -1,14 +1,23 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var Summoner = new Schema({
+  name:   String,
+  region:  String,
+  summonerId: Number,
+});
+
+
 var Streamer = new Schema({
   name:   String,
   channelName:  String,
   online: Boolean,
   viewers: Number,
   preview: String,
+  summoners: [Summoner],
   create_date: { type: Date, default: Date.now }
 
 });
+
 
 module.exports = mongoose.model('Streamer', Streamer);

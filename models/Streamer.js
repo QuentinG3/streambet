b780de83441996c94840ee861a232bd6158eddf2
RectoO <nicolas.vrielynck@student.uclouvain.be>
2016-04-02 19:08:18 +0200

    add function commafy

--- models/Streamer.js
+++ models/Streamer.js
@@ -11,4 +11,19 @@ var Streamer = new Schema({
 
 });
 
+Streamer.methods.viewersComma = function(){
+  return commafy(this.viewers);
+}
+
+function commafy( num ) {
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
+}
+
 module.exports = mongoose.model('Streamer', Streamer);
